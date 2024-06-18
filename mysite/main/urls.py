89a693cac55e17b67bf24f7c8173e002bcca782c
romamyronenko <eoma575@gmail.com>
2024-06-18 20:50:16 +0300

    add form

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index),
     path("tasks/", views.tasks),
     path("task/<int:id>", views.task),
+    path("create/", views.create),
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from main.models import Task
@@ -18,3 +18,17 @@ def task(request, id):
     task = Task.objects.get(id=id)
 
     return render(request, "main/task.html", {"task": task})
+
+
+def create(request):
+    data = {}
+    if request.method == "POST":
+        data = dict(request.POST)
+        task = Task.objects.create(
+            title=data.get('title', [None])[0],
+            description=data.get('description', [None])[0],
+            is_done=data.get('is_done', [None])[0] == 'on',
+        )
+        task.save()
+        return HttpResponseRedirect(f"/task/{task.id}")
+    return render(request, "main/create.html", {'data': data})
