e2606448227fa089add8c4e575f59aeece95b876
romamyronenko <eoma575@gmail.com>
2024-06-18 20:05:15 +0300

    base site

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    is_done = models.BooleanField()
+
+    def __str__(self):
+        return f'[{self.id}] {self.title} | {self.description}'
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from main.models import Task
+
+
 # Create your views here.
+def index(request):
+    return render(request, "main/base.html", {})
+
+
+def tasks(request):
+    tasks = Task.objects.all()
+    return render(request, "main/tasks.html", {"tasks": tasks})
+
+
+def task(request, id):
+    task = Task.objects.get(id=id)
+
+    return render(request, "main/task.html", {"task": task})
