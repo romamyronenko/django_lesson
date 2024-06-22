5e0c427699c12a7eeea21018414b744ed2f165c2
romamyronenko123 <eoma575@gmail.com>
2024-06-22 11:28:09 +0300

    add test

--- mysite/main/tests.py
+++ mysite/main/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.test import TestCase, RequestFactory
+from django.urls import reverse, resolve
+
+from main.views import index
+
 
 # Create your tests here.
+class ToDoListTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username="TestUser", email='', password='pass'
+        )
+
+    def test_index(self):
+        request = self.factory.get("/")
+        request.user = self.user
+        response: HttpResponse = index(request)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, f"<h1>{self.user.username}</h1>")
+
+    def test_url(self):
+        url = reverse('main:home')
+
+        self.assertEqual(resolve(url).func, index)
+        self.assertEqual(url, "/")
